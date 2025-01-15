e61e95677952f1aedb814d71658cd8e2535f81c3
dxb1l <dxb1l@int.pl>
2025-01-15 18:22:23 +0100

    asd

--- offsets.cs
+++ offsets.cs
@@ -1,5 +1,5 @@
 // Generated using https://github.com/a2x/cs2-dumper
-// 2025-01-15 01:32:57.826403300 UTC
+// 2025-01-15 17:22:04.221992300 UTC
 
 namespace CS2Dumper.Offsets {
     // Module: client.dll
--- pulse_system_dll.rs
+++ pulse_system_dll.rs
@@ -1,5 +1,5 @@
 // Generated using https://github.com/a2x/cs2-dumper
-// 2025-01-15 01:32:57.826403300 UTC
+// 2025-01-15 17:22:04.221992300 UTC
 
 #![allow(non_upper_case_globals, non_camel_case_types, non_snake_case, unused)]
 
