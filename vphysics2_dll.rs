934af96e3fa9ccdd20ca7beebe8f9226c410de53
dxb1l <dxb1l@int.pl>
2025-03-25 13:53:23 +0100

    asdsad

--- vphysics2_dll.rs
+++ vphysics2_dll.rs
@@ -1,5 +1,5 @@
 // Generated using https://github.com/a2x/cs2-dumper
-// 2025-03-22 23:23:12.684657 UTC
+// 2025-03-25 12:51:39.015127500 UTC
 
 #![allow(non_upper_case_globals, non_camel_case_types, non_snake_case, unused)]
 
