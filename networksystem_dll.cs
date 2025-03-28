1b01e3f888bb45845a9c07f207ee0e63109065d1
dxb1l <dxb1l@int.pl>
2025-03-28 17:05:25 +0100

    sadsadasd

--- networksystem_dll.cs
+++ networksystem_dll.cs
@@ -1,5 +1,5 @@
 // Generated using https://github.com/a2x/cs2-dumper
-// 2025-03-26 18:10:39.094330400 UTC
+// 2025-03-28 16:05:02.375195800 UTC
 
 namespace CS2Dumper.Schemas {
     // Module: networksystem.dll
