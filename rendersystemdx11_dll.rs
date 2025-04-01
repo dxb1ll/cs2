92b6c771651006b0806364bb63ba052453e798d6
dxb1l <dxb1l@int.pl>
2025-04-01 17:20:23 +0200

    ddddddd

--- rendersystemdx11_dll.rs
+++ rendersystemdx11_dll.rs
@@ -1,5 +1,5 @@
 // Generated using https://github.com/a2x/cs2-dumper
-// 2025-04-01 15:15:06.094645500 UTC
+// 2025-04-01 15:19:38.524155600 UTC
 
 #![allow(non_upper_case_globals, non_camel_case_types, non_snake_case, unused)]
 
--- worldrenderer_dll.cs
+++ worldrenderer_dll.cs
@@ -1,5 +1,5 @@
 // Generated using https://github.com/a2x/cs2-dumper
-// 2025-04-01 15:15:06.094645500 UTC
+// 2025-04-01 15:19:38.524155600 UTC
 
 namespace CS2Dumper.Schemas {
     // Module: worldrenderer.dll
